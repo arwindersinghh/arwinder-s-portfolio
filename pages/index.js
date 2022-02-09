2336822aa118053a7f92a3872dc4ec71cbcff11b
Arwinder Singh <arwindersinghh@gmail.com>
2022-02-09 00:08:16 -0500

    sticky navbar

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ const NavLink = ({ children }) => (
 );
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>            
+      <Box zIndex={200} sx={{ position: 'sticky', top: '0' }} bg={useColorModeValue('gray.100', 'gray.900')} px={4}>            
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
         <IconButton
             size={'md'}
